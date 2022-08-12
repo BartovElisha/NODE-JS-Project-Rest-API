eaadfbdd68ac00498187cb16b966b938f6652ec7
ElishaBartov <elisha.bartov@emerson.com>
2022-08-12 23:25:20 +0300

    Finished Task, Part 10.2

--- middleware/checkToken.js
+++ middleware/checkToken.js
@@ -1,21 +1,24 @@
 const jwt = require('jsonwebtoken');
  
 module.exports = (req, res, next) => {
+    const token = req.header('x-auth-token');
 
-  const token = req.header('x-auth-token');
-  if (!token) {
-    return res.status(401).send('Access denied. No token provided.');
-  } 
+    if(!token) {
+        return res.status(401).send('Access denied. No token provided.');
+    } 
  
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_PASSWORD);
-    if(!decoded || !decoded.id || !decoded.biz) {   
-        return res.status(401).send('Token is invalid or expired.') };
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_PASSWORD);
+        if(!decoded || !decoded.id) {   
+        //if(!decoded || !decoded.id || !decoded.biz) {   
+            console.log(decoded);
+            return res.status(401).send('Token is invalid or expired.'); 
+        }
         req.uid = decoded.id;
         req.biz = decoded.biz;
         next();
-  }
-  catch (error) {
-    res.status(400).send('Invalid token.');
-  }
+    }
+    catch (error) {
+        res.status(400).send('Invalid token.');
+    }
 }
--- routes/card.js
+++ routes/card.js
@@ -3,6 +3,7 @@ const morgan = require('morgan');  // Importing the morgan module (for logs)
 
 const _ = require('lodash');  // Load the full build.
 const chalk = require('chalk');  // Importing the chalk module
+const jwt = require('jsonwebtoken');
 
 const router = express.Router();
 const checkToken = require('./../middleware/checkToken');
@@ -27,7 +28,15 @@ async function createRequest(req, res) {
     // Debug Print req.body 
     console.log(chalk.blue(`Data recieved from POST Methode:`));
     console.log(req.body);
-    res.status(200).send(req.body);
+
+    if(req.biz) {
+        console.log(chalk.green(`Business user can create new card`));
+        res.status(200).send(req.body);
+    }
+    else {
+        console.log(chalk.red(`Regular user not allowed to create new card !!!`));
+        res.status(400).send("Regular user not allowed to create new card !!!");
+    }    
 }
 
 module.exports = router;
